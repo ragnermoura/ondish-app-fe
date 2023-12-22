86f520a5e8cebfd87a402b5cd501b57ccd4d3684
Daniel Brenner <brenner@softwareprecisao.com.br>
2023-12-22 20:42:08 -0300

    Partners and search components

---
+++ src/components/cards/search/searchComp.js
@@ -0,0 +1,128 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components/native';
+import config from '../../../../assets/config.json'
+import { StyleSheet, Text, ImageBackground, View, Image} from 'react-native'
+import { render } from 'react-native-web';
+
+
+
+const styles = StyleSheet.create({
+
+
+  main: {
+    width: '46%',
+    marginRight: '2%',
+    marginLeft: '2%',
+    marginBottom: 30,
+    height: 288,
+  },
+
+  imageConfig: {
+    MaxWidth: 160,
+    height: 220,
+    resizeMode: 'center',
+    borderRadius: 15,
+    overflow: "hidden",    
+  },
+
+  container: {
+    flex:4,
+    MaxWidth: 160,
+    Height: 220,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    borderWidth: 0
+  },
+
+  boxTitle: {
+  },
+
+  title: {
+    color: `${config.colors.black}`,
+    fontSize: 20,
+    letterSpacing: 0.44,
+    fontWeight: 'bold',
+    marginTop: 15
+  },
+
+  subTitle: {
+    color: `${config.colors.placeholder}`,
+    fontSize: 16,
+    letterSpacing: 0.4,
+    marginTop: 5,
+    lineHeight: 24,
+  },
+
+  iconFast: {
+    width: 20,
+    height: 20,
+    position: 'absolute',
+    bottom: 50,
+    left: 10
+  },
+
+  textFast: {
+    position: 'absolute',
+    bottom: 51,
+    left: 35,
+    fontSize: 14,
+    color: '#fff'
+  },
+
+  iconFree: {
+    width: 20,
+    height: 20,
+    position: 'absolute',
+    bottom: 20,
+    left: 10,
+    display: 'block'
+  },
+
+  textFree: {
+    position: 'absolute',
+    bottom: 21,
+    left: 35,
+    fontSize: 14,
+    color: '#fff'
+  },
+
+  rating: {
+    width: 50,
+    padding: 3,
+    backgroundColor: `${config.colors.red}`,
+    position: 'absolute',
+    bottom: 19,
+    right: 10,
+    color: '#fff',
+    textAlign: 'center',
+    borderRadius: 5
+  }
+
+})
+
+
+
+const Banner = ({background, title, tags, iconFree, iconFast, rating, time, price }) => {
+    return (
+
+      <View style={styles.main}>
+        <ImageBackground source={{uri: background}} style={styles.imageConfig} >
+          <View style={styles.container}>
+            <Image source={{uri: iconFast}} style={styles.iconFast}/>
+            <Text style={styles.textFast}>{time}</Text>
+            <Image source={{uri: iconFree}} style={styles.iconFree}/>
+            <Text style={styles.textFree}>{price}</Text>
+            <Text style={styles.rating}>{rating}</Text>
+          </View>
+        </ImageBackground>
+
+          <View style={styles.boxTitle}>
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.subTitle}>{tags}</Text>   
+          </View>
+        </View>
+      
+    );
+}
+
+export default Banner;
--- src/screens/splashscreen/splashscreen.js
+++ src/screens/splashscreen/splashscreen.js
@@ -1,24 +1,105 @@
 import React, { useEffect, useState } from 'react';
 import { Container, ListBanner } from './styles';
 import { useRoute } from '@react-navigation/native';
-// import {Image} from 'react-native';
+import { StyleSheet, Text, ImageBackground, View, Image} from 'react-native'
 
-import Banner from '../../components/cards/ads/adsComp'
+import styled from 'styled-components/native';
 
+// import Banner from '../../components/cards/ads/adsComp'
+// import Banner from '../../components/cards/partners/partnersComp'
+import Banner from '../../components/cards/search/searchComp'
 
+const MasterContainer = styled.View`
+  margin-top: 50px;
+  flex: 1;
+  flex-direction: row;
+  flex-wrap: wrap;
+  text-align: left;
+
+`;
 
 
 export default () => {
 
-  const obj = [
+  // const obj = [
+  //   {
+  //     id: 1,
+  //     title: 'Entrega gratuita por 1 mês',
+  //     subtitle: 'Você precisa fazer um pedido de pelo menos 10€ para usar a entrega gratuita por 1 mês.',
+  //     pic: 'https://lh3.googleusercontent.com/pw/ABLVV875iZNjYImoDp1tyeOnqVcF9FUoBVE4swJiTt2bAiALMqpJ3-vswLIjeBuLnZfFxOnkELF-Z6JsuZAeJ8LC9IyEVuvUjSyRK_dV_GnDm08DbRLNonYPF12R1piw_Ev45YkZPPG5jZwurLdoQ0KbPrrv=w364-h185-s-no-gm?authuser=0'
+  //   },
+  
+  // ]
+
+  // const partners = [
+  //   {
+  //     id: 1,
+  //     title: "McDonald's",
+  //     tags: 'Chinese / American',
+
+  //     iconFree: "https://softwareprecisao.com.br/ondish/iconFreeDelivery.png",
+
+  //     iconFast: 'https://softwareprecisao.com.br/ondish/iconDeliveryFast_.png',
+      
+
+  //     time: '25 Minutos',
+  //     price: 'FREE',
+  //     rating: 4.5,
+  //     background: 'https://www.farofamagazine.com.br/wp-content/uploads/2022/01/mat-20191021190905mc-donalds.jpg',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "KFC",
+  //     tags: 'Brazilian / American',
+
+  //     iconFree: "https://softwareprecisao.com.br/ondish/iconFreeDelivery.png",
+
+  //     iconFast: 'https://softwareprecisao.com.br/ondish/iconDeliveryFast_.png',
+      
+
+  //     time: '25 Minutos',
+  //     price: 'FREE',
+  //     rating: 4.9,
+  //     background: 'https://mercadoeconsumo.com.br/wp-content/uploads/2022/10/International-Meal-Company-firma-novo-acordo-para-uso-da-marca-KFC-shutterstock_1308118438-scaled.jpg',
+  //   },
+
+  // ]
+
+  const search = [
     {
       id: 1,
-      title: 'Entrega gratuita por 1 mês',
-      subtitle: 'Você precisa fazer um pedido de pelo menos 10€ para usar a entrega gratuita por 1 mês.',
-      pic: 'https://lh3.googleusercontent.com/pw/ABLVV875iZNjYImoDp1tyeOnqVcF9FUoBVE4swJiTt2bAiALMqpJ3-vswLIjeBuLnZfFxOnkELF-Z6JsuZAeJ8LC9IyEVuvUjSyRK_dV_GnDm08DbRLNonYPF12R1piw_Ev45YkZPPG5jZwurLdoQ0KbPrrv=w364-h185-s-no-gm?authuser=0'
+      title: "Nethai Kitchen",
+      tags: 'Chinese / American',
+
+      iconFree: "https://softwareprecisao.com.br/ondish/iconFreeDelivery.png",
+
+      iconFast: 'https://softwareprecisao.com.br/ondish/iconDeliveryFast_.png',
+      
+
+      time: '25 Minutos',
+      price: 'FREE',
+      rating: 4.5,
+      background: 'https://www.findandeat.com.br/wp-content/uploads/2017/06/lahuellamesa.jpg',
     },
-  
+    {
+      id: 2,
+      title: "Lazy Bear",
+      tags: 'Brazilian / American',
+
+      iconFree: "https://softwareprecisao.com.br/ondish/iconFreeDelivery.png",
+
+      iconFast: 'https://softwareprecisao.com.br/ondish/iconDeliveryFast_.png',
+      
+
+      time: '25 Minutos',
+      price: 'FREE',
+      rating: 4.9,
+      background: 'https://www.wzhoteljardins.com.br/wp-content/uploads/2020/10/WZ-Hotel-Jardins-955-Lounge-e-Restaurante-19.jpg',
+    },
+
   ]
+
+  
 
 
 
@@ -30,13 +111,32 @@ export default () => {
 
 
     <Container>
+
+      {/* ADS COMP 
       {obj.map(item => (
         <Banner 
         key={item.id} title={item.title} 
         subtitle={item.subtitle}
         pic={item.pic} 
         />
+      ))}*/}
+      <MasterContainer>
+      {search.map(item =>(
+        <Banner
+          key={item.id}
+          background={item.background}
+          title={item.title}
+          tags={item.tags}
+          iconFast={item.iconFast}
+          time={item.time}
+          iconFree={item.iconFree}
+          price={item.price}
+          rating={item.rating}
+        />
       ))}
+      </MasterContainer>
+
+
     </Container>
   );
 };
