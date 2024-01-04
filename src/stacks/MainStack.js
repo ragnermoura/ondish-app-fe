747e878cca93c81012d17f9ff4334c279cd4e3e7
Ragner Moura <44169840+ragnermoura@users.noreply.github.com>
2024-01-04 00:47:24 +0000

    ✅ - Base Criação de telas

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -2,6 +2,10 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Splashscreen from "../screens/splashscreen/splashscreen";
+import Login from "../screens/auth/login";
+import Redefinir from "../screens/auth/redefinir";
+import Recuperar from "../screens/auth/recuperar";
+import Registar from "../screens/auth/registar";
 
 const Stack = createStackNavigator();
 
@@ -12,6 +16,10 @@ export default () => (
             headerShown: false,
         }}>
         <Stack.Screen name="Splashscreen" component={Splashscreen} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Redefinir" component={Redefinir} />
+        <Stack.Screen name="Recuperar" component={Recuperar} />
+        <Stack.Screen name="Registar" component={Registar} />
 
     </Stack.Navigator>
 );
